e17662324485b565577c569b08b91f73306c52de
gponce <gponce@cys.com.ar>
2020-01-19 00:34:43 -0300

    agrega comportamiento al filtro por estrellas

--- front/src/app/buscador/components/filtros/filtro-estrellas/filtro-estrellas.component.ts
+++ front/src/app/buscador/components/filtros/filtro-estrellas/filtro-estrellas.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'filtro-estrellas',
@@ -7,13 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FiltroEstrellasComponent implements OnInit {
 
+  @Output("onFilterChange") onFilterChange: EventEmitter<any> = new EventEmitter();
   cantidadEstrellasAFiltrar: Array<number>;
+  stars: Array<number>;
   
   constructor() { 
     this.cantidadEstrellasAFiltrar = [5,4,3,2,1];
+    this.stars = []
   }
   
   ngOnInit() {
   }
 
+  toggleEstrellaFilter(filtroSeleccionado, checked) {
+    if(checked) {
+      this.stars = [...this.stars, filtroSeleccionado]
+    }
+    else {
+      this.stars = this.stars.filter(estrellas => estrellas !== filtroSeleccionado)
+    }
+
+    this.onFilterChange.emit({ stars: this.stars })
+  }
+
+  selectAll() {
+    this.stars = []
+    this.onFilterChange.emit({ stars: this.stars })
+  }
+
+  isSelected(estrella) {
+    return this.stars.find(e => e === estrella)
+  }
 }
